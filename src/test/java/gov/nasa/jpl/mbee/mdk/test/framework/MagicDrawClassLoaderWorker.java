8efc28b3bb81ba0244c47189104916cc27d51fdd
Paul Gaspardo <gaspardo@jpl.nasa.gov>
2021-09-11 12:55:11 -0700

    Fix MagicDrawClassLoaderWorker
     * Update the testCompile Gradle library dependencies to use the same version as the build (6.5.4) instead of 3.5.1
     * Update MagicDrawClassLoaderWorker to match https://github.com/gradle/gradle/blob/v5.6.4/subprojects/worker-processes/src/main/java/org/gradle/process/internal/worker/child/SystemApplicationClassLoaderWorker.java

--- src/test/java/gov/nasa/jpl/mbee/mdk/test/framework/MagicDrawClassLoaderWorker.java
+++ src/test/java/gov/nasa/jpl/mbee/mdk/test/framework/MagicDrawClassLoaderWorker.java
@@ -18,13 +18,17 @@ package gov.nasa.jpl.mbee.mdk.test.framework;
 
 import org.gradle.api.Action;
 import org.gradle.api.logging.LogLevel;
+import org.gradle.initialization.GradleUserHomeDirProvider;
 import org.gradle.internal.UncheckedException;
+import org.gradle.internal.concurrent.CompositeStoppable;
+import org.gradle.internal.concurrent.DefaultExecutorFactory;
 import org.gradle.internal.concurrent.ExecutorFactory;
 import org.gradle.internal.event.DefaultListenerManager;
 import org.gradle.internal.event.ListenerManager;
 import org.gradle.internal.io.ClassLoaderObjectInputStream;
 import org.gradle.internal.logging.LoggingManagerInternal;
 import org.gradle.internal.logging.services.LoggingServiceRegistry;
+import org.gradle.internal.nativeintegration.services.NativeServices;
 import org.gradle.internal.remote.MessagingClient;
 import org.gradle.internal.remote.ObjectConnection;
 import org.gradle.internal.remote.internal.inet.MultiChoiceAddress;
@@ -42,16 +46,23 @@ import org.gradle.process.internal.health.memory.JvmMemoryStatus;
 import org.gradle.process.internal.health.memory.JvmMemoryStatusListener;
 import org.gradle.process.internal.health.memory.MemoryManager;
 import org.gradle.process.internal.health.memory.OsMemoryInfo;
-import org.gradle.process.internal.worker.WorkerLoggingSerializer;
-import org.gradle.process.internal.worker.WorkerJvmMemoryInfoSerializer;
+import org.gradle.process.internal.worker.child.DefaultWorkerDirectoryProvider;
 import org.gradle.process.internal.worker.child.WorkerContext;
+import org.gradle.process.internal.worker.child.WorkerDirectoryProvider;
 import org.gradle.process.internal.worker.child.WorkerJvmMemoryInfoProtocol;
 import org.gradle.process.internal.worker.child.WorkerLogEventListener;
 import org.gradle.process.internal.worker.child.WorkerLoggingProtocol;
+import org.gradle.process.internal.worker.WorkerJvmMemoryInfoSerializer;
+import org.gradle.process.internal.worker.WorkerLoggingSerializer;
 
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.ObjectInputStream;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.Callable;
 
 /**
@@ -59,7 +70,7 @@ import java.util.concurrent.Callable;
  * care of deserializing and invoking the worker action.</p>
  *
  * <p> Instantiated in the implementation ClassLoader and invoked from {@link org.gradle.process.internal.worker.GradleWorkerMain}.
- * See {@link ApplicationClassesInSystemClassLoaderWorkerFactory} for details.</p>
+ * See {@link ApplicationClassesInSystemClassLoaderWorkerImplementationFactory} for details.</p>
  */
 public class MagicDrawClassLoaderWorker implements Callable<Void> {
     private final DataInputStream configInputStream;
@@ -68,6 +79,7 @@ public class MagicDrawClassLoaderWorker implements Callable<Void> {
         this.configInputStream = configInputStream;
     }
 
+    @Override
     public Void call() throws Exception {
         if (System.getProperty("org.gradle.worker.test.stuck") != null) {
             // Simulate a stuck worker. There's probably a way to inject this failure...
@@ -79,18 +91,31 @@ public class MagicDrawClassLoaderWorker implements Callable<Void> {
 
         // Read logging config and setup logging
         int logLevel = decoder.readSmallInt();
-        LoggingManagerInternal loggingManager = createLoggingManager();
-        loggingManager.setLevelInternal(LogLevel.values()[logLevel]).start();
+        LoggingServiceRegistry loggingServiceRegistry = LoggingServiceRegistry.newEmbeddableLogging();
+        LoggingManagerInternal loggingManager = createLoggingManager(loggingServiceRegistry).setLevelInternal(LogLevel.values()[logLevel]);
 
         // Read whether process info should be published
         boolean shouldPublishJvmMemoryInfo = decoder.readBoolean();
 
+        // Read path to Gradle user home
+        String gradleUserHomeDirPath = decoder.readString();
+        File gradleUserHomeDir = new File(gradleUserHomeDirPath);
+
         // Read server address and start connecting
         MultiChoiceAddress serverAddress = new MultiChoiceAddressSerializer().read(decoder);
-        MessagingServices messagingServices = new MessagingServices();
-        MagicDrawClassLoaderWorker.WorkerServices workerServices = new MagicDrawClassLoaderWorker.WorkerServices(messagingServices);
+        NativeServices.initialize(gradleUserHomeDir, false);
+        DefaultServiceRegistry basicWorkerServices = new DefaultServiceRegistry(NativeServices.getInstance(), loggingServiceRegistry);
+        basicWorkerServices.add(ExecutorFactory.class, new DefaultExecutorFactory());
+        basicWorkerServices.addProvider(new MessagingServices());
+        final WorkerServices workerServices = new WorkerServices(basicWorkerServices, gradleUserHomeDir);
+
+        ObjectConnection connection = null;
 
         WorkerLogEventListener workerLogEventListener = null;
+        File workingDirectory = workerServices.get(WorkerDirectoryProvider.class).getWorkingDirectory();
+        File errorLog = getLastResortErrorLogFile(workingDirectory);
+        PrintUnrecoverableErrorToFileHandler unrecoverableErrorHandler = new PrintUnrecoverableErrorToFileHandler(errorLog);
+
         try {
             // Read serialized worker
             byte[] serializedWorker = decoder.readBinary();
@@ -104,36 +129,74 @@ public class MagicDrawClassLoaderWorker implements Callable<Void> {
                 throw UncheckedException.throwAsUncheckedException(e);
             }
 
-            final ObjectConnection connection = messagingServices.get(MessagingClient.class).getConnection(serverAddress);
+            connection = basicWorkerServices.get(MessagingClient.class).getConnection(serverAddress);
+            connection.addUnrecoverableErrorHandler(unrecoverableErrorHandler);
             workerLogEventListener = configureLogging(loggingManager, connection);
+            // start logging now that the logging manager is connected
+            loggingManager.start();
             if (shouldPublishJvmMemoryInfo) {
                 configureWorkerJvmMemoryInfoEvents(workerServices, connection);
             }
 
-            try {
-                action.execute(new WorkerContext() {
-                    public ClassLoader getApplicationClassLoader() {
-                        //return ClassLoader.getSystemClassLoader();
-                        return getClass().getClassLoader();
-                    }
+            final ObjectConnection serverConnection = connection;
+            action.execute(new WorkerContext() {
+                @Override
+                public ClassLoader getApplicationClassLoader() {
+                    //return ClassLoader.getSystemClassLoader();
+                    return getClass().getClassLoader();
+                }
 
-                    @Override
-                    public ObjectConnection getServerConnection() {
-                        return connection;
-                    }
-                });
-            } finally {
-                connection.stop();
-            }
+                @Override
+                public ObjectConnection getServerConnection() {
+                    return serverConnection;
+                }
+
+                @Override
+                public ServiceRegistry getServiceRegistry() {
+                    return workerServices;
+                }
+            });
         } finally {
-            if (workerLogEventListener != null) {
-                loggingManager.removeOutputEventListener(workerLogEventListener);
+            try {
+                if (workerLogEventListener != null) {
+                    loggingManager.removeOutputEventListener(workerLogEventListener);
+                }
+                CompositeStoppable.stoppable(connection, basicWorkerServices).stop();
+                loggingManager.stop();
+            } catch (Throwable t) {
+                // We're failing while shutting down, so log whatever might have happened.
+                unrecoverableErrorHandler.execute(t);
             }
-            messagingServices.close();
-            loggingManager.stop();
         }
 
         return null;
+    }
+
+    private File getLastResortErrorLogFile(File workingDirectory) {
+        return new File(workingDirectory, "worker-error-" + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + ".txt");
+    }
+
+    private static class PrintUnrecoverableErrorToFileHandler implements Action<Throwable> {
+        private final File errorLog;
+
+        private PrintUnrecoverableErrorToFileHandler(File errorLog) {
+            this.errorLog = errorLog;
+        }
+
+        @Override
+        public void execute(Throwable throwable) {
+            try {
+                final PrintStream ps = new PrintStream(errorLog);
+                try {
+                    ps.println("Encountered unrecoverable error:");
+                    throwable.printStackTrace(ps);
+                } finally {
+                    ps.close();
+                }
+            } catch (FileNotFoundException e) {
+                // ignore this, we won't be able to get any logs
+            }
+        }
     }
 
     private WorkerLogEventListener configureLogging(LoggingManagerInternal loggingManager, ObjectConnection connection) {
@@ -144,7 +207,7 @@ public class MagicDrawClassLoaderWorker implements Callable<Void> {
         return workerLogEventListener;
     }
 
-    private void configureWorkerJvmMemoryInfoEvents(MagicDrawClassLoaderWorker.WorkerServices services, ObjectConnection connection) {
+    private void configureWorkerJvmMemoryInfoEvents(WorkerServices services, ObjectConnection connection) {
         connection.useParameterSerializers(WorkerJvmMemoryInfoSerializer.create());
         final WorkerJvmMemoryInfoProtocol workerJvmMemoryInfoProtocol = connection.addOutgoing(WorkerJvmMemoryInfoProtocol.class);
         services.get(MemoryManager.class).addListener(new JvmMemoryStatusListener() {
@@ -155,15 +218,25 @@ public class MagicDrawClassLoaderWorker implements Callable<Void> {
         });
     }
 
-    LoggingManagerInternal createLoggingManager() {
-        LoggingManagerInternal loggingManagerInternal = LoggingServiceRegistry.newEmbeddableLogging().newInstance(LoggingManagerInternal.class);
+    LoggingManagerInternal createLoggingManager(LoggingServiceRegistry loggingServiceRegistry) {
+        LoggingManagerInternal loggingManagerInternal = loggingServiceRegistry.newInstance(LoggingManagerInternal.class);
         loggingManagerInternal.captureSystemSources();
         return loggingManagerInternal;
     }
 
     private static class WorkerServices extends DefaultServiceRegistry {
-        public WorkerServices(ServiceRegistry... parents) {
-            super(parents);
+        public WorkerServices(ServiceRegistry parent, final File gradleUserHomeDir) {
+            super(parent);
+            addProvider(new Object() {
+                GradleUserHomeDirProvider createGradleUserHomeDirProvider() {
+                    return new GradleUserHomeDirProvider() {
+                        @Override
+                        public File getGradleUserHomeDirectory() {
+                            return gradleUserHomeDir;
+                        }
+                    };
+                }
+            });
         }
 
         ListenerManager createListenerManager() {
@@ -180,6 +253,10 @@ public class MagicDrawClassLoaderWorker implements Callable<Void> {
 
         MemoryManager createMemoryManager(OsMemoryInfo osMemoryInfo, JvmMemoryInfo jvmMemoryInfo, ListenerManager listenerManager, ExecutorFactory executorFactory) {
             return new DefaultMemoryManager(osMemoryInfo, jvmMemoryInfo, listenerManager, executorFactory);
+        }
+
+        WorkerDirectoryProvider createWorkerDirectoryProvider(GradleUserHomeDirProvider gradleUserHomeDirProvider) {
+            return new DefaultWorkerDirectoryProvider(gradleUserHomeDirProvider);
         }
     }
 }
